43e6b031fd42419e112c3408fd7db7efd5a36486
olgagrishaeva <olga.gr2812@mail.ru>
2023-06-28 21:10:33 +0300

    рабочие тесты в нужной папке

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ import static org.example.utils.AddressDb.*;
 public class Main {
     public static void main(String[] args) {
         ParseXMLFile parseXMLFile = new ParseXMLFile();
-        parseXMLFile.parse(new File("DoubleAddress//address.xml"));
+        parseXMLFile.parse(new File("address.xml"));
         System.out.println("1 task");
         DoubleAddress doubleAddress = new DoubleAddress();
         doubleAddress.doubleAddressLine(addressList);
